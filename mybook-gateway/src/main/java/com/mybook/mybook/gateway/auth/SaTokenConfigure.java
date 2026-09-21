4ad8f3def257213a3e74c6dc338d649bc5afa1d5
Gallop <1274224114@qq.com>
2026-09-21 11:20:11 +0800

    some changes

--- mybook-gateway/src/main/java/com/mybook/mybook/gateway/auth/SaTokenConfigure.java
+++ mybook-gateway/src/main/java/com/mybook/mybook/gateway/auth/SaTokenConfigure.java
@@ -27,6 +27,7 @@ public class SaTokenConfigure {
                     SaRouter.match("/**")
                             .notMatch("/auth/verification/code/send")
                             .notMatch("/auth/login")
+                            .notMatch("/actuators")
                             .check(r -> StpUtil.checkLogin());
                     SaRouter.match("/auth/logout")
                             .check(r -> StpUtil.checkRole("admin"));
